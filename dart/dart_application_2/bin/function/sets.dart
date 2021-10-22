56054c93633e15adbd86ef80106fc0ce7a5602ef
qasem-moh <2171246@s.aou.edu.jo>
2021-10-23 01:06:03 +0300

    dart

--- dart/dart_application_2/bin/function/sets.dart
+++ dart/dart_application_2/bin/function/sets.dart
@@ -2,7 +2,8 @@ void main() {
   var x = {1, 2, 3, 4, 5, 6, 7, 8, 9};
   Set y = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-  x.forEach((element) { //loop forEach item in object
+  x.forEach((element) {
+    //loop forEach item in object
     print(element);
   });
   print('#' * 20);
@@ -12,4 +13,9 @@ void main() {
 
   print(z);
   print(z.runtimeType); // print type of z
+
+  var a = {4, 5, 6, 7};
+  var b = {6, 7, 8, 9};
+  print(a.intersection(b));
+  print(a.union(b));
 }
