4c3bba088a0b9fa63a9971e94feaf5751ea0607a
qasem-moh <2171246@s.aou.edu.jo>
2021-10-22 16:54:20 +0300

    dart

---
+++ dart/dart_application_2/bin/function/lists.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<int> li = [1, 2, 3, 5, 6, 7, 87];
+    print(li);
+  // List<String> li2 = ['1','1','1','1',];
+  // List<double> li3 = [1, 2, 3, 5, 6, 7, 87];
+  // List<dynamic> li4 = [1, 2, 3, 5, 6, 7, 87];
+}
