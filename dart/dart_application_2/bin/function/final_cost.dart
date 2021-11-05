0b7931ef9b05d88d0a4d8210a3f06a30c0b2da51
qasem-moh <2171246@s.aou.edu.jo>
2021-11-06 01:17:22 +0200

    dart

--- dart/dart_application_2/bin/function/final_cost.dart
+++ dart/dart_application_2/bin/function/final_cost.dart
@@ -6,5 +6,10 @@ void main() {
 
   final int a = DateTime.now().year;
   // const int a = DateTime.now().year;
+  print(a);
 
+  final List a10 = ['a', 'b', 'c'];
+  const List a11 = ['a', 'b', 'c'];
+  a11.add('v');
+  print(a11);
 }
