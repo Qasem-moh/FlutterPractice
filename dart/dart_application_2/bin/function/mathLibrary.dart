dfffcb8a6cea59ea17ca52e29b2c98e0d478cf8d
qasem-moh <2171246@s.aou.edu.jo>
2021-11-07 16:23:12 +0200

    dart

---
+++ dart/dart_application_2/bin/function/mathLibrary.dart
@@ -0,0 +1,8 @@
+import 'dart:math';
+
+void main() {
+  print('sin (30) = ${sin(30 * (pi / 180))}');
+  print('cos (60) = ${cos(60 * (pi / 180))}');
+  print('tan (60) = ${tan(60 * (pi / 180))}');
+  print('asin(0.5) =${asin(0.5 * (180 / pi))}');
+}
